354767b0f17b0ea12e55c7779ef53355ba3e38a1
haiseong <wjdgotjd9908@gmail.com>
2024-05-27 17:29:05 +0900

    2015

---
+++ acmicpc/2015.py
@@ -0,0 +1,21 @@
+
+n, k = map(int, input().split())
+lst = list(map(int, input().split()))
+sums = [0] * (n)
+
+for i in range(n):
+    sums[i] = sums[i - 1] + lst[i]
+
+answer = 0
+
+sums_dict = {}
+for s in sums:
+    if s - k in sums_dict:
+        answer += sums_dict[s - k]
+    if s == k:
+        answer+=1
+
+    if s not in sums_dict:
+        sums_dict[s] = 0
+    sums_dict[s] += 1
+print(answer)
