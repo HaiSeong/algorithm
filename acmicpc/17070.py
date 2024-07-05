dcd99f9f3ed36d0ca9e4dd558bc9bbdb64ab27fa
haiseong <wjdgotjd9908@gmail.com>
2024-07-05 11:41:24 +0900

    17070.py

--- acmicpc/17070.py
+++ acmicpc/17070.py
@@ -28,3 +28,55 @@ for i in range(n):
                 dp[i][j][RIGHT_DOWN] += dp[i - 1][j - 1][RIGHT_DOWN]
 
 print(sum(dp[n-1][n-1]))
+
+
+'''
+# 아루의 재귀
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+constexpr int INF = 0x3f3f3f3f;
+constexpr ll LINF = 0x3f3f3f3f3f3f3f3f;
+
+int N, dp[4][20][20], g[20][20], y, x;
+
+// k : 1 (가로), 2(세로), 3(대각) 
+int f(int k, int y, int x) {
+	if (y == N-1 && x == N-1) return g[y][x] == 0;
+	if (g[y][x] == 1) return 0;
+	if (y >= N || x >= N) return 0;
+	
+	int& ret = dp[k][y][x];
+	if (ret != -1) return ret;
+	
+	ret = 0;
+	if (k == 1) {
+		if (g[y][x+1] == 0) ret += f(k, y, x+1);
+		if (g[y][x+1] + g[y+1][x] + g[y+1][x+1] == 0) ret += f(3, y+1, x+1);
+	}
+	else if (k == 2) {
+		if (g[y+1][x] == 0) ret += f(k, y+1, x);
+		if (g[y][x+1] + g[y+1][x] + g[y+1][x+1] == 0) ret += f(3, y+1, x+1);
+	}
+	else {
+		if (g[y][x+1] + g[y+1][x] + g[y+1][x+1] == 0) ret += f(k, y+1, x+1);
+		if (g[y][x+1] == 0) ret += f(1, y, x+1);
+		if (g[y+1][x] == 0) ret += f(2, y+1, x);
+	}
+	
+	return ret;
+}
+
+int main(){
+	cin.tie(nullptr)->sync_with_stdio(false);
+	memset(dp, -1, sizeof dp);
+	
+	cin >> N;
+	for (int i = 0; i < N; i++) 
+		for (int j = 0; j < N; j++) cin >> g[i][j];
+		
+	cout << f(1, 0, 1) << '\n';
+	
+	return 0;
+}
+'''
