bb9689defc4310a3fbe20d2e705d376f8d22d667
haiseong <wjdgotjd9908@gmail.com>
2025-07-21 11:06:19 +0900

    디렉토리 정리

---
+++ practice/python_to_java/io.py
@@ -0,0 +1,21 @@
+
+s = input()
+
+# BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+# String line = br.readLine();
+
+# StringTokenizer st = new StringTokenizer(br.readLine());
+# String a = st.nextToken()
+
+# BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+# bw.write("123");
+# bw.newLine();
+# bw.flush();
+# bw.close();
+
+# StringBuilder sb = new StringBuilder();
+# for (int i = 0; i < n; i++) {
+#     sb.append(st.nextToken());
+#     sb.append("\n");
+# }
+# System.out.println(sb.toString());
