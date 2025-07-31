ec1a64a2a580e1704c1493e53ccf597b02f9ba3e
Haiseong Jeong <58177929+HaiSeong@users.noreply.github.com>
2025-07-31 10:48:10 +0900

    [Silver I] Title: IOIOI, Time: 264 ms, Memory: 24052 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -8,30 +8,25 @@ public class Main {
       int n = Integer.parseInt(br.readLine());
       int m = Integer.parseInt(br.readLine());
       
-      StringBuilder finding_ = new StringBuilder("I");
-      for (int i = 0; i < n; i++){
-        finding_.append("OI");
-      }
-      String finding = finding_.toString();
       String line = br.readLine();
       
-      int cnt = 0;
+      int [] dp = new int [m];
       
-      for (int i = 0; i < m - finding.length() + 1; i++) {
-        
-        boolean same = true;
-        for (int j = 0; j < finding.length(); j ++) {
-          if (line.charAt(i + j) != finding.charAt(j)) {
-            same = false;
-            break;
+      for (int i = 0; i < m; i++) {
+        if (line.charAt(i) == 'I') {
+          dp[i] = 1;
+          if (i - 2 >= 0 && line.charAt(i - 1) == 'O' && line.charAt(i - 2) == 'I') {
+            dp[i] = dp[i - 2] + 1;
           }
         }
-        
-        if (same) {
-          cnt++;
+      }
+      int cnt = 0;
+      for (int i = 0; i < m; i++){
+        // System.out.println(dp[i]);
+        if (dp[i] > n) {
+          cnt += 1;
         }
       }
-      
       System.out.println(cnt);
   }
 }
