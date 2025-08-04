d318a2db89921e0424f025f1adf5a17ac13b729e
Haiseong Jeong <58177929+HaiSeong@users.noreply.github.com>
2025-08-04 11:24:12 +0900

    [level 3] Title: 단속카메라, Time: 2.24 ms, Memory: 9.77 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,28 +1,14 @@
 def solution(routes):
-    answer = 1
+    answer = 0
     
-    routes.sort(key = lambda x : (x[0], -x[1]))
+    routes.sort(key = lambda x : x[1])
+    last = -30001
+    
     print(routes)
-    start, end = -30000, 30000
-    for s, e in routes:
-        if end < start:
-            print(s, e)
-            answer += 1
-            start = s
-            end = e
-            continue
-        
-        if max(s, start) > min(e, end):
-            answer += 1
-            start = s
-            end = e
-            continue
-        
-        
-        start = max(s, start)
-        end = min(e, end)
-        
-        print(start, end)
-            
     
+    for s, e in routes:
+        if s > last:
+            answer += 1
+            last = e
+            
     return answer
